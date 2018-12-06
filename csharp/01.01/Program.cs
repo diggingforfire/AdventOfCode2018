1266010004ae944dd26e7dc403a8d046843ef620
Robbie Luijben <diggingforfire@gmail.com>
2018-12-06 13:04:05 +0100

    Day 1 C#

---
+++ csharp/01.01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01._01
+{
+	class Program
+	{
+		static void Main()
+		{
+			var result = File.ReadAllLines("input.txt").Sum(int.Parse);
+			Console.WriteLine(result);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ csharp/01.02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _01._02
+{
+	class Program
+	{
+		static void Main()
+		{
+			var input = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+			var result = Find(input, new List<int>(), 0);
+
+			Console.WriteLine(result);
+			Console.ReadKey();
+		}
+
+		static int Find(int[] input, List<int> seen, int sum)
+		{
+			for (int i = 0; i < input.Length; i++)
+			{
+				sum += input[i];
+				if (seen.Contains(sum)) return sum;
+				seen.Add(sum);
+			}
+
+			return Find(input, seen, sum);
+		}
+	}
+}
