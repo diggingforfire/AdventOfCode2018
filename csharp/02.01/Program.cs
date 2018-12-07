92807f6569caffd343b41c0d41f7e9319aef69a3
robbie.luijben <robbie.luijben@snappet.org>
2018-12-07 14:11:51 +0100

    Day 2 in C#

---
+++ csharp/02.01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace _02._01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var groups = File.ReadAllLines("input.txt").Select(line => line.GroupBy(@char => @char)).ToList();
+
+			var checksum =
+				groups.Count(charGroup => charGroup.Any(chars => chars.Count() == 2)) *
+				groups.Count(charGroup => charGroup.Any(chars => chars.Count() == 3));
+
+			Console.WriteLine(checksum);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ csharp/02.02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace _02._02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var lines = File.ReadAllLines("input.txt");
+
+			var result = lines
+				.SelectMany(line => lines, (a, b) => new
+				{
+					FirstLine = a,
+					SecondLine = b,
+					Intersection = a.Where((_, i) => a[i] == b[i])
+				})
+				.Where(pair => pair.FirstLine != pair.SecondLine)
+				.First(pair => pair.Intersection.Count() == pair.FirstLine.Length - 1)
+				.Intersection;
+ 
+			Console.WriteLine(new string(result.ToArray()));
+			Console.ReadKey();
+		}
+	}
+}
