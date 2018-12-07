5dc358cbfe23e700f42432d3fa24b93d52750c7c
robbie.luijben <robbie.luijben@snappet.org>
2018-12-07 15:40:18 +0100

    Start of day 3 in C#

---
+++ csharp/03.01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03._01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var claims =
+				File.ReadAllLines("input.txt")
+				.Select(line =>
+				new
+				{
+					Parts = line.Split(new[] { ' ', ',', ':', 'x', '@' }, StringSplitOptions.RemoveEmptyEntries)
+				})
+				.Select(claim =>
+				new
+				{
+					Id = claim.Parts[0],
+					Left = int.Parse(claim.Parts[1]),
+					Top = int.Parse(claim.Parts[2]),
+					Width = int.Parse(claim.Parts[3]),
+					Height = int.Parse(claim.Parts[4])
+				}).ToList();
+
+ 
+ 
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
